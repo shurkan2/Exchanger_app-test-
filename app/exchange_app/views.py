f9b6b9197ffd619c7465781d58f276f771c4fc4b
Eduard <edikzvonarev@gmail.com>
2024-09-14 05:34:23 +0300

    final step

--- app/exchange_app/views.py
+++ app/exchange_app/views.py
@@ -1,41 +1,38 @@
 from django.shortcuts import render
 import requests
 
-def exchange(request):
-    print(request.method)
-    currencies = {}
-    #''' Не работал на GET запросы!!'''
 
-    context = {}
-    response = requests.get(url="https://v6.exchangerate-api.com/v6/0f4f6bd40a93f62532b34f0c/latest/USD").json()
+def exchange(request):
+
+    response = requests.get("https://v6.exchangerate-api.com/v6/0f4f6bd40a93f62532b34f0c/latest/USD").json()
     currencies = response.get('conversion_rates', {})
 
-    if request.method == 'GET':
-        if currencies:
-            context = {
-                'currencies': currencies
-            }
+    context = {
+        'currencies': currencies
+    }
 
-        return render(request=request, template_name='exchange_app/index.html', context=context)
 
     if request.method == 'POST':
-        #Своя логика
-        return render(request=request, template_name='exchange_app/index.html', context=context)
-        from_amount = request.POST.get('from_amount')
+        from_amount = request.POST.get('from-amount')
         from_curr = request.POST.get('from-curr')
         to_curr = request.POST.get('to-curr')
-        try:
-            converted_amount = round(currencies[to_curr]/currencies[from_curr] * float(from_amount), 2)
-            context['converted_amount'] = converted_amount
-        except KeyError:
-            print(fr'Не,нихуч')
-        finally:
-            context = {
-                'from_amount': from_amount,
-                'from_curr': from_curr,
-                'to_curr': to_curr,
-                'currencies': currencies
-            }
-        return render(request=request, template_name='exchange_app/index.html', context=context)
-        pass
 
+        if from_amount and from_curr and to_curr:
+            try:
+                from_amount = float(from_amount)
+
+                converted_amount = round((currencies[to_curr] / currencies[from_curr]) * from_amount, 2)
+
+                context.update({
+                    'from_curr': from_curr,
+                    'to_curr': to_curr,
+                    'from_amount': from_amount,
+                    'converted_amount': converted_amount
+                })
+
+            except ValueError:
+                context['error'] = "Некорректное значение суммы."
+        else:
+            context['error'] = "Пожалуйста, заполните все поля."
+
+    return render(request=request, template_name='exchange_app/index.html', context=context)
